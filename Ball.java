996db802ccb51b508d930c15f9f64b097b63dd92
parth <35137360+parth2324@users.noreply.github.com>
2019-11-17 08:57:57 +0530

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -2,6 +2,15 @@ package Project.BallDesign;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.util.ArrayList;
+/*
+Version 1.
+Author : Parth Singh.
+This class is a simple blueprint of a 'displayable' ball.
+-> coordinate(x,y) stored, movement direction stored -> dir(x,y) -> 1 or -1.
+-> sp(x,y) -> speed i.e. increase in (x,y) on each update.
+-> color of ball -> java.awt.Color c;
+-> vicinity() returns all main Ball[] indexes, which are at a distance less than the minimum distance entered.
+*/
 class Ball
 {
     int x,y,dirx,diry,spx,spy;
